517ee60a266c7a2df1c8747c882d5dab237d099c
Андрей Небышинец <andneb03@gmail.com>
2019-05-06 03:34:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Div, Group, Panel, PanelHeader, View} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <View activePanel="mainPanel">
+                <Panel id="mainPanel">
+                    <PanelHeader>Example</PanelHeader>
+                    <Group>
+                        <Div>Hello world</Div>
+                    </Group>
+                </Panel>
+            </View>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import connect from '@vkontakte/vkui-connect';
+import App from './App';
+
+connect.subscribe((e) => {
+    switch (e.detail.type) {
+        case 'VKWebAppUpdateConfig':
+            let schemeAttribute = document.createAttribute('scheme');
+            schemeAttribute.value = e.detail.data.scheme ? e.detail.data.scheme : 'client_light';
+            document.body.attributes.setNamedItem(schemeAttribute);
+            break;
+
+        default:
+            console.log(e.detail.type);
+    }
+});
+
+// Init VK App
+connect.send('VKWebAppInit', {});
+
+ReactDOM.render(<App />, document.getElementById('root'));
